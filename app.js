6683e2914d280937d20fc49b1f22d07503991c57
hal01001k <hiteshdaksh@yahoo.co.uk>
2020-07-19 01:18:16 +0530

    mongoose connect

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var leaderRouter = require('./routes/leaderRouter');
 var promoRouter = require('./routes/promoRouter');
+ const mongoose = require('mongoose');
+ const Dishes = require('./models/dishes');
+ const url = 'mongodb://localhost:27017/conFusion';
+ const connect = mongoose.connect(url);
+
+ connect.then((db)=>{
+   console.log('Connected to Server');
+ },(err)=>{console.log(err);});
+
+
 var app = express();
 
 // view engine setup
